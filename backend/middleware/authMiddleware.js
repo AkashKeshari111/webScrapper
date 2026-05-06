783b048144b6e7ea8bea4d17c003a4ca8b114d8f
AkashKeshari111 <TechTeam-studentdiwan>
2026-05-07 01:24:36 +0530

    add auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import { sendResponse } from "../utils/response.js";
+import jwt from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    let token;
+
+    if (req.headers.authorization?.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return sendResponse(res, 401, false, "Not authorized, no token found");
+    }
+
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = {
+      id: decoded.id,
+      email: decoded.email,
+    };
+
+    next();
+  } catch (error) {
+    return sendResponse(res, 401, false, "Not authorized, token failed");
+  }
+};
