47f038c56b990567cc445bd21c794ad58d474576
AkashKeshari111 <TechTeam-studentdiwan>
2026-05-07 01:07:25 +0530

    add auth controller,router

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { dbConnect } from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth",authRouter);
+
 app.get("/", (req, res) => {
   res.send({ msg: "Runnig app testing..." });
 });
@@ -16,5 +20,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (req, res) => {
   dbConnect();
-  console.log(`Server running on port https://localhost:${PORT}/`);
+  console.log(`Server running on port http://localhost:${PORT}/`);
 });
---
+++ backend/utils/response.js
@@ -0,0 +1,24 @@
+
+export const sendResponse = (
+  res,
+  statusCode,
+  success,
+  message,
+  data = null,
+  meta = null,
+) => {
+  const response = {
+    success,
+    message,
+  };
+
+  if (data !== null && data !== undefined) {
+    response.data = data;
+  }
+
+  if (meta) {
+    response.meta = meta;
+  }
+
+  return res.status(statusCode).json(response);
+};
