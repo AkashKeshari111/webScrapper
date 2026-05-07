84fd90c65e353d5f43be9e7dabfc2de69a0b2e72
AkashKeshari111 <TechTeam-studentdiwan>
2026-05-07 17:38:36 +0530

    remove unwanted folder

--- backend/routes/scraperRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { scrapeStories } from "../controllers/scraperController.js";
-
-const scraperRouter = express.Router();
-
-scraperRouter.get("/scrape", scrapeStories);
-
-export default scraperRouter;
